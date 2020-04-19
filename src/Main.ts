b5d674f11dccca9cc77e8593d9a367c9e4e25cad
Viktor Hapiak <viktor.hapiak@gmail.com>
2020-04-20 00:14:07 +0300

    Add first executable version

--- src/Main.ts
+++ src/Main.ts
@@ -1,6 +1,20 @@
+/// <reference types="phaser" />
+
+import { GameScene } from "./ui/GameScene";
 
 function main(): void {
 
+    let phaserGame = new Phaser.Game({
+        backgroundColor: 0x472509,
+        scale: {
+            width: 1920,
+            height: 1080,
+            mode: Phaser.Scale.FIT,
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+            parent: 'content'
+        },
+        scene: [GameScene]
+    });
 }
 
 main();
